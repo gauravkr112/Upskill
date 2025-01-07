f6460a07fdf06194c6d1351722838dfab8e4bcd1
gauravkr112 <157815433+gauravkr112@users.noreply.github.com>
2025-01-07 17:38:37 +0530

    Create variables.tf

---
+++ modules/iam/variables.tf
@@ -0,0 +1,11 @@
+variable "role_name" {
+  description = "The name of the IAM role for Systems Manager"
+}
+
+variable "policy_attachment_name" {
+  description = "The name of the IAM policy attachment"
+}
+
+variable "instance_profile_name" {
+  description = "The name of the IAM instance profile"
+}
